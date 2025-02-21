2171293c571864cc848acc2c85d5742cde1bb6d8
hansajasandeepabadalge <hansajasandeepabadalge@gmail.com>
2025-02-21 16:17:54 +0530

    Can convert other languages

--- csv2psv.py
+++ csv2psv.py
@@ -5,7 +5,7 @@ os.makedirs('output', exist_ok=True)
 
 filepath = 'file.csv'
 
-with open(filepath, 'r') as infile, open('output/output.csv', 'w', newline='') as outfile:
+with open(filepath, 'r', encoding='utf-8') as infile, open('output/output.csv', 'w', newline='', encoding='utf-8') as outfile:
     reader = csv.reader(infile, delimiter=',')
     writer = csv.writer(outfile, delimiter='|')
     writer.writerows(reader)
